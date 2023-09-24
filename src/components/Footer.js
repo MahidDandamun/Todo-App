30e50abcf694307f365cab2d036a91b8dc0c9ba9
Mahid Dandamun <dandamunmahid@gmail.com>
2023-09-24 22:42:22 +0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React, { useContext, useState } from 'react'
+import { DataContext } from './DataProvider'
+
+export default function Footer() {
+  const [checkAll, setCheckAll] = useState(false)
+  const [todos, setTodos] = useContext(DataContext)
+
+  const handleCheckAll = () => {
+    const newTodos = [...todos]
+    newTodos.forEach(todo => {
+      todo.complete = !checkAll
+    })
+    setTodos(newTodos)
+    setCheckAll(!checkAll)
+  }
+
+  const newTodosComplete = () => {
+    return todos.filter(todo => todo.complete === false)
+  }
+
+  const deleteTodo = () => {
+    setTodos(newTodosComplete())
+    setCheckAll(false)
+  }
+
+  return (
+    <>
+      {
+        todos.length === 0 ? <h2 className='nothingTodo'>Congrats Mahid! you have no upcoming to-do</h2>
+          : <div className="row" >
+            <label htmlFor="all">
+              <input type="checkbox" name="all" id="all"
+                onChange={handleCheckAll} checked={checkAll} />
+              <span class="allText">All</span>
+            </label>
+            <p class="remainingTodo">You have <span class="remainingTodoNum">{todos.filter(todo => todo.complete === false).length}</span>  todo.</p>
+            <button id="delete" onClick={deleteTodo}>Delete</button>
+          </div>
+      }
+    </>
+
+
+  )
+}
